3e6ce0396785326154b1f404ac247fe3ffae3ee7
张沪东 <shrugor@163.com>
2025-10-10 17:06:27 +0800

    cc

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 from transformers import PretrainedConfig
 import torch.nn as nn
+import torch
+
 
 '''定义超参数'''
 class ModelConfig(PretrainedConfig):
@@ -31,10 +33,56 @@ class ModelConfig(PretrainedConfig):
         self.dropout = dropout
         self.flash_attn = flash_attn
         super().__init__(**kwargs)
-    
+
 class RMSNorm(nn.Module):
     def __init__(self, dim: int, eps: float):
         super().__init__()
         # eps是为了防止除以0的情况
         self.eps = eps
-        # 
+        # weight是一个可学习的参数，全部初始化为1
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x):
+        # 计算RMSNorm的核心部分
+        # x.pow(2).mean(-1, keepdim=True)计算了输入x的平方的均值
+        # torch.rsqrt是平方根的倒数，这样就得到了RMSNorm的分母部分，再加上eps防止分母为0
+        # 最后乘以x，得到RMSNorm的结果
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        # forward函数是模型的前向传播
+        # 首先将输入x转为float类型，然后进行RMSNorm，最后再转回原来的数据类型
+        # 最后乘以weight，这是RMSNorm的一个可学习的缩放因子
+        output = self._norm(x.float()).type_as(x)
+        return output * self.weight
+    
+
+'''分组查询注意力机制'''
+def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
+    # 获取输入张量的形状：批量大小、序列长度、键/值对头的数量、每个头的维度大小
+    bs, slen, n_kv_heads, head_dim = x.shape
+
+    # 如果重复次数为1，则不需要重读，直接返回原始张量
+    if n_rep == 1:
+        return x
+    
+    # 对张量进行扩展和重塑操作以重复键值对
+    return (
+        x[:, :, :, None, :]  # 在第四个维度添加一个新的维度
+        .expend(bs, slen, n_kv_heads, n_rep, head_dim)  # 将新添加的维度扩展到n_rep大小，实现重复效果
+        .reshape(bs, slen, n_kv_heads*n_rep, head_dim)  # 重新塑性，合并键值对头的数量和重复次数的维度
+    )
+
+'''旋转嵌入'''
+# 注意：此处的dim应为dim//n_head， 因为我们是对
+def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
+
+
+
+if __name__ == "__main__":
+
+    args = ModelConfig()
+    norm = RMSNorm(args.dim, args.norm_eps)
+    x = torch.randn(1, 50, args.dim)
+    output = norm(x)
+    print(output.shape)
