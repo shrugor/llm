b806d49aa278fcb19bd26356c4a8c80518ed5694
张沪东 <shrugor@163.com>
2025-10-11 13:38:55 +0800

    cc

--- model.py
+++ model.py
@@ -55,7 +55,68 @@ class RMSNorm(nn.Module):
         # 最后乘以weight，这是RMSNorm的一个可学习的缩放因子
         output = self._norm(x.float()).type_as(x)
         return output * self.weight
+
+
+'''旋转嵌入'''
+# 注意：此处的dim应为dim//n_head， 因为我们是对每个head进行旋转嵌入
+def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
+    # torch.arange(1, dim ,2)[:(dim//2)].float()生成了一个从0开始，步长为2的序列。长度为dim的一半
+    # 然后每个元素除以dim，再取theta的指数后取倒数，得到频率
+    freqs = 1.0 / (theta ** (torch.arange(0, dim , 2)[:(dim//2)].float()/dim))
+    # 生成一个从0到end的序列，长度为end，通常是序列的最大长度
+    t = torch.arange(end, device=freqs.device)
+    # 计算外积，得到二维矩阵，每一行是t的元素乘以freqs的元素
+    freqs = torch.outer(t, freqs).float()
+    # 计算频率的余弦，得到实部
+    freqs_cos = torch.cos(freqs)
+    # 计算频率的正弦，得到虚部
+    freqs_sin = torch.sin(freqs)
+    return freqs_cos, freqs_sin
+
+def reshape_for_broadcast(freqs_cis: torch.Tensor, x:torch.Tensor):
+    # 获取x的维度数
+    ndim = x.ndim
     
+    # 断言，确保1在x维度范围内
+    assert 0 <= 1 < ndim
+
+    # 断言，确保freqs_cis的形状与x的第二维和最后一维相同
+    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
+
+    # 构造一个新的形状，除了第二维和最后一维，其它维度都是1，这样做也是为了能够将freqs_cis与x进行广播操作
+    shape = [d if i == 1 or i == ndim - 1 else 1 for i , d in enumerate(x.shape)]
+
+    # 将freqs_cis调整为新的形状，并返回
+
+    return freqs_cis.view(shape)
+
+def apply_rotary_emb(
+        xq: torch.Tensor,
+        xk: torch.Tensor,
+        freqs_cos: torch.Tensor,
+        freqs_sin: torch.Tensor,
+) -> tuple[torch.Tensor, torch.Tensor]:
+    
+    # 将查询和键张量转换为浮点数，并重塑形状以分离实部和虚部
+    xq_r, xq_i = xq.float().reshape(xq.shape[:-1] + (-1 ,2)).unbind(-1)
+    xk_r, xk_i = xk.float().reshape(xk.shape[:-1] + (-1, 2)).unbind(-1)
+
+    # 重新塑性频率张量以进行广播
+    freqs_cos = reshape_for_broadcast(freqs_cos, xq_r)
+    freqs_sin = reshape_for_broadcast(freqs_sin, xq_r)
+
+    # 应用旋转，分别计算旋转后的实部和虚部
+    xq_out_r = xq_r * freqs_cos - xq_i * freqs_sin
+    xq_out_i = xq_r * freqs_cos + xq_i * freqs_cos
+    xk_out_r = xk_r * freqs_cos - xk_i * freqs_sin
+    xk_out_i = xk_r * freqs_sin + xk_i * freqs_cos
+
+    # 将最后两个维度合并，并还原为原始张量的形状
+    xq_out = torch.stack([xq_out_r, xq_out_i], dim = -1).flatten(3)
+    xk_out = torch.stack([xk_out_r, xk_out_i], dim = -1).flatten(3)
+
+    return xq_out.type_as(xq), xk_out.type_as(xk)    
+
 
 '''分组查询注意力机制'''
 def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
@@ -73,15 +134,21 @@ def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
         .reshape(bs, slen, n_kv_heads*n_rep, head_dim)  # 重新塑性，合并键值对头的数量和重复次数的维度
     )
 
-'''旋转嵌入'''
-# 注意：此处的dim应为dim//n_head， 因为我们是对
-def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
-    pass
 
 if __name__ == "__main__":
 
-    args = ModelConfig()
-    norm = RMSNorm(args.dim, args.norm_eps)
-    x = torch.randn(1, 50, args.dim)
-    output = norm(x)
-    print(output.shape)
+    # test norm
+    # args = ModelConfig()
+    # norm = RMSNorm(args.dim, args.norm_eps)
+    # x = torch.randn(1, 50, args.dim)
+    # output = norm(x)
+    # print(output.shape)
+
+
+    # test RoPe
+    xq = torch.randn(1, 50, 6, 48)
+    xk = torch.randn(1, 50, 6, 48)
+    cos, sin = precompute_freqs_cis(288//6, 50)
+    print(cos.shape, sin.shape)
+    xq_out, xk_out = apply_rotary_emb(xq, xk, cos, sin)
+    print(xq_out.shape, xk_out.shape)
