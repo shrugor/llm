dc6de4eeb22ea622eab55a7599229f98e849bd49
张沪东 <shrugor@163.com>
2025-10-13 15:04:15 +0800

    cc

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 from transformers import PretrainedConfig
 import torch.nn as nn
 import torch
+import math
+import torch.nn.functional as F
 
 
 '''定义超参数'''
@@ -130,19 +132,101 @@ def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
     # 对张量进行扩展和重塑操作以重复键值对
     return (
         x[:, :, :, None, :]  # 在第四个维度添加一个新的维度
-        .expend(bs, slen, n_kv_heads, n_rep, head_dim)  # 将新添加的维度扩展到n_rep大小，实现重复效果
+        .expand(bs, slen, n_kv_heads, n_rep, head_dim)  # 将新添加的维度扩展到n_rep大小，实现重复效果
         .reshape(bs, slen, n_kv_heads*n_rep, head_dim)  # 重新塑性，合并键值对头的数量和重复次数的维度
     )
 
 class Attention(nn.Module):
     def __init__(self, args: ModelConfig):
         super().__init__()
-        # 根据是否指定n_
+        # 根据是否指定n_kv_heads，确定用于键（key）和值（value）的头的数量
+        self.n_kv_heads = args.n_heads if args.n_kv_heads is None else args.n_kv_heads
+        # 确保总头数可以呗键值头数整除
+        assert args.n_heads % self.n_kv_heads == 0
+
+        # 模型并行处理大小，默认为1
+        model_parallel_size = 1
+        # 本地计算头数，等于总头数处理模型并行处理大小
+        self.n_local_heads = args.n_heads // model_parallel_size
+        # 本地键值头数，等于键值头数除以模型并行处理大小
+        self.n_local_kv_heads = self.n_kv_heads // model_parallel_size
+        # 重复次数，用于扩展键值的尺寸
+        self.n_rep = self.n_local_heads // self.n_local_kv_heads
+        # 每个头的维度，等于模型维度除以头的总数
+        self.head_dim = args.dim // args.n_heads
+
+        # 定义权重矩阵
+        self.wq = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
+        self.wk = nn.Linear(args.dim, self.n_kv_heads * self.head_dim, bias=False)
+        self.wv = nn.Linear(args.dim, self.n_kv_heads * self.head_dim, bias=False)
+        # 输出权重矩阵
+        self.wo = nn.Linear(args.n_heads * self.head_dim, args.dim, bias=False)
+
+        # 定义dropout
+        self.attn_dropout = nn.Dropout(args.dropout)
+        self.resid_dropout = nn.Dropout(args.dropout)
+        # 保存dropout概率
+        self.dropout = args.dropout
+
+        # 检查是否使用Flash Attention (需要PyTorch >= 2.0)
+        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
+        if not self.flash:
+            # 若不支持Flash Attention，则使用手动实现的注意力机制，并设置mask
+            print("WARNING: using slow attention, Flash Attention requires PyTorch >= 2.0")
+            # 创建一个上三角矩阵，用于遮蔽未来信息
+            mask = torch.full((1, 1, args.max_seq_len, args.max_seq_len), float("-inf"))
+            mask = torch.triu(mask, diagonal=1)
+
+    def forward(self, x: torch.Tensor, freqs_cos: torch.Tensor, freqs_sin: torch.Tensor):
+        # 获取批次大小和序列长度，[batch_size, seq_len, dim]
+        bsz, seqlen, _ = x.shape
+
+        # 计算查询（Q）， 键（K），值（V）
+        xq, xk, xv = self.wq(x), self.wk(x), self.wv(x)
+        # 调整形状以适应头的维度
+        xq = xq.view(bsz, seqlen, self.n_local_heads, self.head_dim)
+        xk = xk.view(bsz, seqlen, self.n_local_kv_heads, self.head_dim)
+        xv = xv.view(bsz, seqlen, self.n_local_kv_heads, self.head_dim)
+
+        # 应用旋转位置嵌入(RoPe)
+        xq, xk = apply_rotary_emb(xq, xk, freqs_cos, freqs_sin)
+
+        # 对键和值进行扩展以适应重复次数
+        xk = repeat_kv(xk, self.n_rep)
+        xv = repeat_kv(xv, self.n_rep)
+
+        # 将头作为批次维度处理
+        xq = xq.transpose(1, 2)
+        xk = xk.transpose(1, 2)
+        xv = xv.transpose(1, 2)
+
+        # 根据是否支持Flash Attention
+        if self.flash:
+            # 使用Flash Attention
+            output = torch.nn.functional.scaled_dot_product_attention(xq, xk, xv, attn_mask=None, dropout_p=self.dropout if self.training else 0.0, is_causal=True)
+        else:
+            # 使用手动实现的注意力机制
+            scores = torch.matmul(xq, xk.transpose(2, 3)) / math.sqrt(self.head_dim)
+            assert hasattr(self, "mask")
+            scores = scores + self.mask[:, :, :seqlen, :seqlen]
+            scores= F.softmax(scores.float(), dim=-1).type_as(xq)
+            scores = self.attn_dropout(scores)
+            output = torch.matmul(scores, xv)
+        
+        # 恢复时间维度并合并头
+        output= output.transpose(1, 2).contiguous().view(bsz, seqlen, -1)
+
+        # 最终投影回残差流
+        output = self.wo(output)
+        output = self.resid_dropout(output)
+        return output
+
 
 if __name__ == "__main__":
 
+    args = ModelConfig()
+
     # test norm
-    # args = ModelConfig()
     # norm = RMSNorm(args.dim, args.norm_eps)
     # x = torch.randn(1, 50, args.dim)
     # output = norm(x)
@@ -150,9 +234,21 @@ if __name__ == "__main__":
 
 
     # test RoPe
-    xq = torch.randn(1, 50, 6, 48)
-    xk = torch.randn(1, 50, 6, 48)
-    cos, sin = precompute_freqs_cis(288//6, 50)
-    print(cos.shape, sin.shape)
-    xq_out, xk_out = apply_rotary_emb(xq, xk, cos, sin)
-    print(xq_out.shape, xk_out.shape)
+    # xq = torch.randn(1, 50, 6, 48)
+    # xk = torch.randn(1, 50, 6, 48)
+    # cos, sin = precompute_freqs_cis(288//6, 50)
+    # print(cos.shape, sin.shape)
+    # xq_out, xk_out = apply_rotary_emb(xq, xk, cos, sin)
+    # print(xq_out.shape, xk_out.shape)
+
+    # test attention
+    attention_model = Attention(args)
+    batch_size = 1
+    seq_len= 50
+    dim = args.dim
+    x = torch.rand(batch_size, seq_len, dim)
+    freqs_cos, freqs_sin = precompute_freqs_cis(dim//args.n_heads, seq_len)
+    output = attention_model(x, freqs_cos, freqs_sin)
+    print(output.shape)
+
+    # 
